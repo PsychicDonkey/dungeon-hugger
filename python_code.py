cddca802060f940b7d93b32cb34cb487fe0de55b
PsychicDonkey <160005684+PsychicDonkey@users.noreply.github.com>
2024-09-03 20:13:47 +0200

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -12,3 +12,15 @@ def retrieve_city_key_value_database(city_name, key):
         if city['name'] == city_name:
             output=get_key_value_from_city(city,key)
     return output
+
+#function to replace
+def modify_city_key_value(city_name, key, value):
+    output = f"{city_name} doesn't exist or is not covered."
+    for city in cities:
+        if city['name'] == city_name:
+            if key in city.keys():
+                city[key]= value
+                output = f"{key} has been updated for {city_name}."
+            else:
+                output = f"{key} doesn't exist."
+    return output
